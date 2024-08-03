4b4ff26d93c8cd5471b7c7a4104c3116b5f215e6
elimf <90334513+elimf@users.noreply.github.com>
2024-08-03 17:38:09 +0200

    refactor: Update Navbar component layout and styles

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,64 +1,63 @@
 <template>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark" id="nav">
+      <a class="navbar-brand">Sneakers Addict</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
   
-
-    <nav class="navbar navbar-expand-lg navbar-light bg-dark" id="nav">
-       <a class="navbar-brand" >Sneakers Addict</a>
-        <button
-            class="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-        >
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <router-link to="/">Home</router-link>
-                </li>
-                <li class="nav-item">
-                    <router-link to="/apropos">A Propos</router-link>
-                </li>
-                <li class="nav-item">
-                    <router-link to="/store">Store</router-link>
-                </li>
-            </ul>
-        </div>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <router-link to="/" class="nav-link" active-class="router-link-exact-active">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/apropos" class="nav-link" active-class="router-link-exact-active">A Propos</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/store" class="nav-link" active-class="router-link-exact-active">Store</router-link>
+          </li>
+        </ul>
+      </div>
     </nav>
-</template>
-
-<script></script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style>
-#rownav {
-    display: flex;
-    justify-content: space-between;
-}
-#nav {
+  </template>
+  
+  <script>
+  export default {
+    name: 'Navbar'
+  };
+  </script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+  <style scoped>
+  #nav {
     position: fixed;
     top: 0;
     width: 100%;
     z-index: 10001;
-    background-color: black;
-}
-
-#nav a {
+  }
+  
+  #nav .navbar-brand {
     font-weight: bold;
     color: white;
-    text-decoration: none;
-    margin-right: 30px;
     font-size: 30px;
-}
-
-#nav a.router-link-exact-active {
+  }
+  
+  #nav .nav-link {
+    color: white;
+    font-size: 18px;
+  }
+  
+  #nav .nav-link.router-link-exact-active {
     color: rgb(233, 20, 20);
-    text-decoration: none;
-    margin-right: 40px;
-    font-size: 30px;
-}
-</style>
+    font-weight: bold;
+  }
+  </style>
+  
