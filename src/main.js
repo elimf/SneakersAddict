d7783b3a71af2cd3a11a4e42facd343ebd5bf212
elimf <90334513+elimf@users.noreply.github.com>
2024-08-03 17:37:50 +0200

    chore: Update Bootstrap and Bootstrap-Vue-3 versions and import statements

--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+// Import Bootstrap and Bootstrap-Vue-3 CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css';
 
+// Import Bootstrap and Bootstrap-Vue-3 JS files (optional)
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-createApp(App).use(router).mount('#app')
+// Import Bootstrap-Vue-3
+import BootstrapVue3 from 'bootstrap-vue-3';
+
+const app = createApp(App);
+
+app.use(router);
+app.use(BootstrapVue3);
+
+app.mount('#app');
